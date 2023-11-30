7c9f8705a9ae6203050121418b0964a74e4ec405
philpokr <149322783+philpokr@users.noreply.github.com>
2023-11-29 21:37:48 -0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,24 @@
+﻿using GroupProject.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace GroupProject
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+           services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
