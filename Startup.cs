da09b77083f4677e7dab42b9cf99da6a1e218608
philpokr <149322783+philpokr@users.noreply.github.com>
2023-12-02 22:32:08 -0500

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,10 @@
-﻿using GroupProject.Models;
+using GroupProject.Models;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NSwag;
 
 namespace GroupProject
 {
@@ -16,9 +19,36 @@ namespace GroupProject
 
         public void ConfigureServices(IServiceCollection services)
         {
-
-           services.AddDbContext<ApplicationDbContext>(options =>
+            services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddControllers();
+
+            services.AddSwaggerDocument(config =>
+            {
+                config.Title = "GroupProject API";
+                config.Version = "v1";
+                config.Description = "API for GroupProject";
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GroupProject API v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
