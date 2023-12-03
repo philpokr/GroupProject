6dfa898bb51564a6756541bbc63264531b13bc38
philpokr <149322783+philpokr@users.noreply.github.com>
2023-12-02 22:13:00 -0500

    Add files via upload

---
+++ Controllers/FavoriteFoodsController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GroupProject.Models;
+
+namespace GroupProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FavoriteFoodsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FavoriteFoodsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/FavoriteFoods
+        [HttpGet]
+        public ActionResult<IEnumerable<FavoriteFoods>> GetFavoriteFoods()
+        {
+            return _context.FavoriteFoods.ToList();
+        }
+
+        // GET: api/FavoriteFoods/5
+        [HttpGet("{id}")]
+        public ActionResult<FavoriteFoods> GetFavoriteFood(int id)
+        {
+            var favoriteFood = _context.FavoriteFoods.Find(id);
+
+            if (favoriteFood == null)
+            {
+                return NotFound();
+            }
+
+            return favoriteFood;
+        }
+
+        // POST: api/FavoriteFoods
+        [HttpPost]
+        public IActionResult PostFavoriteFood(FavoriteFoods favoriteFood)
+        {
+            _context.FavoriteFoods.Add(favoriteFood);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetFavoriteFood", new { id = favoriteFood.FoodId }, favoriteFood);
+        }
+
+        // PUT: api/FavoriteFoods/5
+        [HttpPut("{id}")]
+        public IActionResult PutFavoriteFood(int id, FavoriteFoods favoriteFood)
+        {
+            if (id != favoriteFood.FoodId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(favoriteFood).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FavoriteFoodExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/FavoriteFoods/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteFavoriteFood(int id)
+        {
+            var favoriteFood = _context.FavoriteFoods.Find(id);
+
+            if (favoriteFood == null)
+            {
+                return NotFound();
+            }
+
+            _context.FavoriteFoods.Remove(favoriteFood);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        private bool FavoriteFoodExists(int id)
+        {
+            return _context.FavoriteFoods.Any(e => e.FoodId == id);
+        }
+    }
+}
---
+++ Controllers/HobbiesController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GroupProject.Models;
+
+namespace GroupProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HobbiesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HobbiesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Hobbies
+        [HttpGet]
+        public ActionResult<IEnumerable<Hobby>> GetHobbies()
+        {
+            return _context.Hobbies.ToList();
+        }
+
+        // GET: api/Hobbies/5
+        [HttpGet("{id}")]
+        public ActionResult<Hobby> GetHobby(int id)
+        {
+            var hobby = _context.Hobbies.Find(id);
+
+            if (hobby == null)
+            {
+                return NotFound();
+            }
+
+            return hobby;
+        }
+
+        // POST: api/Hobbies
+        [HttpPost]
+        public IActionResult PostHobby(Hobby hobby)
+        {
+            _context.Hobbies.Add(hobby);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetHobby", new { id = hobby.HobbyId }, hobby);
+        }
+
+        // PUT: api/Hobbies/5
+        [HttpPut("{id}")]
+        public IActionResult PutHobby(int id, Hobby hobby)
+        {
+            if (id != hobby.HobbyId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(hobby).State = EntityState.Modified;
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HobbyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Hobbies/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteHobby(int id)
+        {
+            var hobby = _context.Hobbies.Find(id);
+
+            if (hobby == null)
+            {
+                return NotFound();
+            }
+
+            _context.Hobbies.Remove(hobby);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        private bool HobbyExists(int id)
+        {
+            return _context.Hobbies.Any(e => e.HobbyId == id);
+        }
+    }
+}
+
