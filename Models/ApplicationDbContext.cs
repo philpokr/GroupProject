ed9b6c5b290578a92312d9495242fdffa254934a
JowonM <124507544+JowonM@users.noreply.github.com>
2023-12-05 15:55:36 -0500

    Update ApplicationDbContext.cs

--- Models/ApplicationDbContext.cs
+++ Models/ApplicationDbContext.cs
@@ -7,6 +7,9 @@
         public DbSet<TeamMember> TeamMembers { get; set; }
         public DbSet<Hobby> Hobbies { get; set; }
         public DbSet<FavoriteFoods> FavoriteFoods { get => favoriteFoods; set => favoriteFoods = value; }
+        public DbSet<DreamJob> DreamJobs { get; set; }
+        public DbSet<FavoriteSport> FavoriteSports { get; set; }
+        
 
         // Constructor with DbContextOptions parameter
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
