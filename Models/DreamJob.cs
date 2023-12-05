eaae6f24bc77b06c17ecf25cb4a620bdee9ccb59
JowonM <124507544+JowonM@users.noreply.github.com>
2023-12-05 14:34:52 -0500

    Here are my models

---
+++ Models/DreamJob.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GroupProject.Models
+{
+    public class DreamJob
+    {
+        [Key]
+        public int Salary { get; set; }
+
+        public string? JobTitle { get; set; }
+        public string? JobDescription { get; set; }
+        public decimal Rating { get; set; }
+        public string? Benefits { get; set; }
+        public decimal? PrefferedHours { get; set; }
+    }
+}
---
+++ Models/FavoriteSport.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GroupProject.Models
+{
+    public class FavoriteSport
+    {
+        [Key]
+
+        public string? SportTitle { get; set; }
+        public string? FieldsOfPlay { get; set; }
+        public int NumOfPlayers { get; set; }
+        public DateTime? LastTimeWatchedOrPlayed { get; set; }
+    }
+}
