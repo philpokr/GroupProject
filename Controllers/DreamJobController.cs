615f47c7a2af88704a387c7fa97dfa5278f7711e
JowonM <124507544+JowonM@users.noreply.github.com>
2023-12-05 14:36:11 -0500

    here are my controllers

---
+++ Controllers/DreamJobController.cs
@@ -0,0 +1,152 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FinalProject.Models;
+using GroupProject.Models;
+
+namespace FinalProject.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DreamJobController : ControllerBase
+{
+    private readonly ILogger<CollegeStudentController> _logger;
+    private readonly FinalProjectContext _context;
+
+    public DreamJobController(ILogger<CollegeStudentController> logger, FinalProjectContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    [HttpGet]
+    [Route("BySalary")]
+    [ProducesResponseType(typeof(DreamJob), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+    public IActionResult GetBySalary(int? Salary)
+    {
+        if (Salary == null || Salary == 0) return Ok(_context.CollegeStudents?.ToList().Take(5));
+        try
+        {
+            var job = _context.dreamJobs.Find(Salary);
+            if (job == null)
+            {
+                return NotFound("The requested resource was not found");
+            }
+            return Ok(job);
+        }
+        catch (Exception e)
+        {
+            return Problem(e.Message);
+        }
+    }
+
+    [HttpGet]
+    [Route("All")]
+    [ProducesResponseType(typeof(List<DreamJob>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+    public IActionResult GetAll()
+    {
+        try
+        {
+            if (_context.dreamJobs == null || !_context.dreamJobs.Any())
+                return NotFound("No DreamJobs found in the database");
+            return Ok(_context.dreamJobs?.ToList());
+        }
+        catch (Exception e)
+        {
+            return Problem(e.Message);
+        }
+    }
+
+    [HttpDelete]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+    public IActionResult Delete(int Salary)
+    {
+        try
+        {
+            var job = _context.dreamJobs?.Find(Salary);
+            if (job == null)
+            {
+                return NotFound($"DreamJobs with Salary {Salary} was not found");
+            }
+
+            _context.dreamJobs?.Remove(job);
+            var result = _context.SaveChanges();
+            if (result >= 1)
+            {
+                return Ok("Delete operation was successful");
+            }
+            return Problem("Delete was not successful. Please try again");
+        }
+        catch (Exception e)
+        {
+            return Problem(e.Message);
+        }
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+    public IActionResult Add(DreamJob newDreamJob)
+    {
+        if (newDreamJob.Salary != 0)
+        {
+            return BadRequest("Salary was provided but not needed");
+        }
+        try
+        {
+            _context.dreamJobs?.Add(newDreamJob);
+            var result = _context.SaveChanges();
+            if (result >= 1)
+            {
+                return Ok($"DreamJob {newDreamJob.JobTitle} added successfully");
+            }
+            return Problem("Add was not successful. Please try again");
+        }
+        catch (Exception e)
+        {
+            return Problem(e.Message);
+        }
+    }
+
+    [HttpPut]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+    public IActionResult Put(DreamJob jobToEdit)
+    {
+        if (jobToEdit.Salary < 1)
+        {
+            return BadRequest("Please provide a valid Salary");
+        }
+
+        try
+        {
+            var job = _context.dreamJobs?.Find(jobToEdit.Salary);
+            if (job == null)
+                return NotFound("The DreamJob was not found");
+
+            job.JobTitle = jobToEdit.JobTitle;
+            job.JobDescription = jobToEdit.JobDescription;
+            job.Benefits = jobToEdit.Benefits;
+            job.Rating = jobToEdit.Rating;
+
+            _context.dreamJobs?.Update(job);
+            var result = _context.SaveChanges();
+            if (result >= 1)
+            {
+                return Ok("job edited successfully");
+            }
+            return Problem("Edit was not successful. Please try again");
+        }
+        catch (Exception e)
+        {
+            return Problem(e.Message);
+        }
+    }
+}
---
+++ Controllers/FavoriteSportController.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FinalProject.Models;
+using GroupProject.Models;
+
+namespace FinalProject.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FavoriteSportController : ControllerBase
+{
+    private readonly ILogger<FavoriteSportController> _logger;
+    private readonly FinalProjectContext _context;
+
+    public FavoriteSportController(ILogger<FavoriteSportController> logger, FinalProjectContext context)
+
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    [HttpGet]
+    [Route("ByNumOfPlayers")]
+    [ProducesResponseType(typeof(FavoriteSport), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+    public IActionResult GetBySalary(int? numPlay)
+    {
+        if (numPlay == null || numPlay == 0) return Ok(_context.FavoriteSports?.ToList().Take(5));
+        try
+        {
+            var sport = _context.FavoriteSports.Find(numPlay);
+            if (sport == null)
+            {
+                return NotFound("The requested resource was not found");
+            }
+            return Ok(sport);
+        }
+        catch (Exception e)
+        {
+            return Problem(e.Message);
+        }
+    }
+
+    [HttpGet]
+    [Route("All")]
+    [ProducesResponseType(typeof(List<FavoriteSport>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+    public IActionResult GetAll()
+    {
+        try
+        {
+            if (_context.FavoriteSports == null || !_context.FavoriteSports.Any())
+                return NotFound("No FavoriteSports found in the database");
+            return Ok(_context.FavoriteSports?.ToList());
+        }
+        catch (Exception e)
+        {
+            return Problem(e.Message);
+        }
+    }
+
+    [HttpDelete]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+    public IActionResult Delete(int numPlay)
+    {
+        try
+        {
+            var sport = _context.FavoriteSports?.Find(numPlay);
+            if (sport == null)
+            {
+                return NotFound($"FavoriteSports with {numPlay} Players was not found");
+            }
+
+            _context.FavoriteSports?.Remove(sport);
+            var result = _context.SaveChanges();
+            if (result >= 1)
+            {
+                return Ok("Delete operation was successful");
+            }
+            return Problem("Delete was not successful. Please try again");
+        }
+        catch (Exception e)
+        {
+            return Problem(e.Message);
+        }
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+    public IActionResult Add(FavoriteSport newSport)
+    {
+        if (newSport.NumOfPlayers != 0)
+        {
+            return BadRequest("The Number of players was provided but not needed");
+        }
+        try
+        {
+            _context.FavoriteSports?.Add(newSport);
+            var result = _context.SaveChanges();
+            if (result >= 1)
+            {
+                return Ok($"Sport {newSport.SportTitle} added successfully");
+            }
+            return Problem("Add was not successful. Please try again");
+        }
+        catch (Exception e)
+        {
+            return Problem(e.Message);
+        }
+    }
+
+    [HttpPut]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+    public IActionResult Put(FavoriteSport sportToEdit)
+    {
+        if (sportToEdit.NumOfPlayers < 1)
+        {
+            return BadRequest("Please provide a valid Salary");
+        }
+
+        try
+        {
+            var sport = _context.FavoriteSports?.Find(sportToEdit.NumOfPlayers);
+            if (sport == null)
+                return NotFound("The SPort was not found");
+
+            sport.SportTitle = sportToEdit.SportTitle;
+            sport.FieldsOfPlay = sportToEdit.FieldsOfPlay;
+            sport.NumOfPlayers = sportToEdit.NumOfPlayers;
+            sport.LastTimeWatchedOrPlayed = sportToEdit.LastTimeWatchedOrPlayed;
+
+            _context.FavoriteSports?.Update(sport);
+            var result = _context.SaveChanges();
+            if (result >= 1)
+            {
+                return Ok("Sport edited successfully");
+            }
+            return Problem("Edit was not successful. Please try again");
+        }
+        catch (Exception e)
+        {
+            return Problem(e.Message);
+        }
+    }
+}
